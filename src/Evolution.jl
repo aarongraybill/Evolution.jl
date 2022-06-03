a6ab47789c7d1efd90f284a7c61579aa6c6180c3
Aaron Graybill <aarongraybill12@gmail.com>
2022-06-03 15:36:31 -0400

    fix tests

--- src/Evolution.jl
+++ src/Evolution.jl
@@ -16,8 +16,11 @@ include("RayTracing.jl")
 #Define what a being's environment looks like and how to define a population
 include("Habitat.jl")
 
+#Figure out how a being sees all of the objects in its environment and translates that to
+# the minimal perceived information
 include("Perceive.jl")
 
+# construct neural network and how that lives in a being's brain
 include("Brain.jl")
 
 end
